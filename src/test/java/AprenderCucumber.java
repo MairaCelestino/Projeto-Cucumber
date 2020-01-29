7c20510b3a4f3c002c474221a5366644dc24aacb
MairaCelestino <mairacelestino@hotmail.com>
2020-01-29 22:37:02 +0000

    Created class Runner

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,4 +1,3 @@
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -7,22 +6,17 @@ public class AprenderCucumber {
 
 	@Given("^que criei o arquvio corretamente$")
 	public void que_criei_o_arquvio_corretamente() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		//throw new PendingException();
-		//throw new RuntimeException();
-		System.out.println("passou aqui!");
-		}
+
+	}
 
 	@When("^executá-lo$")
 	public void executá_lo() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+
 	}
 
 	@Then("^a especificação deve finalizar com sucesso$")
 	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+
 	}
 
 }
--- target/test-classes/aprender_cucumber.feature
+++ target/test-classes/aprender_cucumber.feature
@@ -1,11 +1,9 @@
 Feature: Aprender Cucumber
-	Como um aluno
-	Eu quero aprender a utilizar o Cucumber
-	Para que eu possa automatizar critérios de aceitação
+  Como um aluno
+  Eu quero aprender a utilizar o Cucumber
+  Para que eu possa automatizar critérios de aceitação
 
-
-Scenario: Deve executar especificação
-
-	Given que criei o arquvio corretamente
-	When executá-lo
-	Then a especificação deve finalizar com sucesso
+  Scenario: Deve executar especificação
+    Given que criei o arquvio corretamente
+    When executá-lo
+    Then a especificação deve finalizar com sucesso
